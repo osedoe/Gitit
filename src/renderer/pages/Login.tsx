030ad92bfd72ab310214c751d6b1b66e8d5a3c0e
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2020-06-05 09:58:07 +0100

    Check

--- src/renderer/pages/Login.tsx
+++ src/renderer/pages/Login.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 
+const GITHUB_URL = 'https://github.com/login/oauth/authorize';
+
 const Container = styled.div`
     display: flex;
     flex-direction: column;
@@ -8,15 +10,16 @@ const Container = styled.div`
     align-items: center;
 `;
 
-const sendNotification = () =>
-    new Notification('Test', {
+const Button = styled.button``;
+
+const sendNotification = () => {
+    return new Notification('Test', {
         body: 'You are not logged in'
     });
+};
 
 export const Login: FC = () => {
     const [isLogged, setIsLogged] = useState(false);
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
 
     useEffect(() => {
         if (!isLogged) {
@@ -24,31 +27,14 @@ export const Login: FC = () => {
         }
     }, [isLogged]);
 
-    const handleInputChange = (event: any) => {
-        const name = event.target.getAttribute('name');
-        if (name === 'username') {
-            setUsername(event.target.value);
-        } else if (name === 'password') {
-            setPassword(event.target.value);
-        }
+    const handleOnClick = () => {
+        console.log(123);
     };
 
     return (
         <Container>
             <h2>Sign in</h2>
-            <input
-                name="username"
-                placeholder="Username"
-                value={username}
-                onChange={handleInputChange}
-                />
-            <input
-                type="password"
-                name="password"
-                placeholder="password"
-                value={password}
-                onChange={handleInputChange}
-                />
+            <Button onClick={handleOnClick}>LOGIN NOW</Button>
         </Container>
     );
 };
---
+++ src/renderer/utils/Oauth.ts
@@ -0,0 +1,4 @@
+export class Oauth {
+    clientId: string;
+    clientSecret: string;
+}
