961216b63aeaeea04e30360b52b32c1838d2aa58
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2020-09-20 14:06:12 +0100

    Cleanup api

--- src/renderer/API.ts
+++ src/renderer/API.ts
@@ -1,10 +1,19 @@
-import { pollWithAuth, requestWithAuth } from './utils';
+import { requestWithAuth } from './utils';
+
+interface NotificationsOptions {
+  all?: boolean;
+  participating?: boolean;
+  since?: string;
+  before?: string;
+}
+
 
 const API = {
-    getAllNotifications: (all = false, participating = false, since = '', before = '') =>
-        // requestWithAuth(`notifications?all=${all}&participating=${participating}&since=${since}&before=${before}`),
-        pollWithAuth(`notifications?all=${all}&participating=${participating}&since=${since}&before=${before}`),
-    getThread: (threadId: string) => requestWithAuth(`notifications/threads/${threadId}`)
+  getAllNotifications: (authHeader: string, options: NotificationsOptions) => {
+    const { all = false, participating = false, since = '', before = '' } = options;
+    return requestWithAuth(`notifications?all=${all}&participating=${participating}&since=${since}&before=${before}`);
+  },
+  getThread: (threadId: string) => requestWithAuth(`notifications/threads/${threadId}`)
 };
 
 export default API;
